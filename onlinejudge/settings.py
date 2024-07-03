7080d251fcbd14f4aa7be6d14213a6d0547a52f5
minhnbnt <minhnbnt@gmail.com>
2024-07-03 22:59:42 +0700

    refactor: small changes in database config file

--- onlinejudge/settings.py
+++ onlinejudge/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
         "OPTIONS": {
-            "read_default_file": "mysql.cnf",
+            "read_default_file": "mysql.toml",
         },
     }
 }
