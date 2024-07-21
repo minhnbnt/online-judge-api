63a969522467029174779920b4a6d0e21f0ca70e
minhnbnt <minhnbnt@gmail.com>
2024-07-22 00:24:35 +0700

    refactor: use django global config

--- onlinejudge/settings.py
+++ onlinejudge/settings.py
@@ -96,6 +96,14 @@ SIMPLE_JWT = {
     "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
 }
 
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 30,
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
