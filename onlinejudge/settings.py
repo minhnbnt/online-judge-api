9f3d3104859cb12b0bfb7880aba732a09cf3c37d
minhnbnt <minhnbnt@gmail.com>
2024-09-23 20:44:20 +0700

    feat: switch to sqlite for easier setup

--- onlinejudge/settings.py
+++ onlinejudge/settings.py
@@ -86,10 +86,8 @@ WSGI_APPLICATION = "onlinejudge.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "OPTIONS": {
-            "read_default_file": "mysql.toml",
-        },
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
