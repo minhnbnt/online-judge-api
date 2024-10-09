6edc8a0d7c4718ba5182a6d5fad578a2a5158434
minhnbnt <minhnbnt@gmail.com>
2024-10-09 12:54:05 +0700

    feat: make hello route more useful

--- api/views.py
+++ api/views.py
@@ -2,12 +2,18 @@ from django.conf import settings
 from django.http import HttpResponseRedirect
 
 from rest_framework.decorators import api_view
+from rest_framework.request import Request
 from rest_framework.views import Response
 
 
 @api_view(["GET"])
-def hello(_):
-    return Response({"message": "Hello, world!"})
+def hello(request: Request):
+    message = "Hello, world!"
+
+    if username := request.user.username:
+        message = f"Hello, your username is {username}."
+
+    return Response({"message": message})
 
 
 @api_view(["GET"])
